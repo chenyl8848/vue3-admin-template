14ffeb590cbd757e07e9048d946cf80d2e80d107
cyl <1063415880@qq.com>
2023-06-05 15:47:39 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <h1>hello world</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
